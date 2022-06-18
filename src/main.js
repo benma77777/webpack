6c15594c321ce58b781914650a06ceaf7ed53f52
benma77777 <2533676796@qq.com>
2022-06-18 08:46:43 +0800

    完成分支功能

--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
 import './ben.js'
 import './ma.js'
+import './styles/index.css'
+import './styles/index.less'
+// 引入vue
+import App from './app.vue'
+
+
+import pngSrc from './assets/logo_small.png'
+import gifSrc from './assets/1.gif'
+
+
+import './assets/fonts/iconfont.css'
+const png = document.createElement('img')
+const gif = document.createElement('img')
+png.src = pngSrc
+gif.src = gifSrc
+
+document.body.appendChild(png)
+document.body.appendChild(gif)
+
+const fn = () => {
+    console.log('77777');
+}
+console.log(fn);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,72 @@
 const { join } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './html/index.html',
+            filename: 'index.html'
+        }
+        ),
+        new VueLoaderPlugin()
+    ],
+    mode: 'development',
     entry: "./src/main.js",
     output: {
         path: join(__dirname, 'lib'),
+        filename: 'index.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            // css
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            // less
+            {
+                test: /\.less$/i,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            // 图片
+            {
+                test: /\.(png|gif|jpeg|jpg)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 20 * 1024
+                    }
+                },
+                generator: {
+                    filename: 'images/[hash:6][ext]'
+                }
+            },
+            // 字体图标
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[name].[hash:6][ext]'
+                }
+            },
+            // 高级语法兼容
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: ["babel-loader"]
+            },
+            // vue
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    devServer: {
+        port: 30000,
+        open: true
     }
 }
